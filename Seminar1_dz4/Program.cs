dc91a9b79d9f7f66ebd95235c7f0d02a422a8040
Yana <petersyana@rambler.ru>
2022-12-02 20:24:25 +0300

    Task 4

---
+++ Seminar1_dz4/Program.cs
@@ -0,0 +1,30 @@
+﻿// программа выводит большее из трех введенных чисел
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.WriteLine("Введите первое число - ");
+string firstNum = Console.ReadLine();
+int num1 = Convert.ToInt32(firstNum);
+
+Console.WriteLine("Введите второе число - ");
+string secondNum = Console.ReadLine();
+int num2 = Convert.ToInt32(secondNum);
+
+Console.WriteLine("Введите третье число - ");
+string threeNum = Console.ReadLine();
+int num3 = Convert.ToInt32(threeNum);
+
+int max = num1;
+
+if (num2 > max)
+    {
+      max = num2;  
+    }
+
+if (num3 > max)
+    {
+      max = num3;  
+    }
+
+Console.WriteLine($"max = {max}");
