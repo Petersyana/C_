6af262e7b48d7d917a0c927a181351260640a221
Yana <petersyana@rambler.ru>
2022-12-02 18:46:25 +0300

    calculate sqr

---
+++ Seminar1_001/Program.cs
@@ -0,0 +1,9 @@
+﻿// пишем программку, которая считает квадрат введенного числа
+
+Console.WriteLine("Enter number: ");
+string enterNum = Console.ReadLine();
+int num = Convert.ToInt32(enterNum);
+
+int sqr = num * num;
+
+Console.WriteLine($"sqr= {sqr}");
