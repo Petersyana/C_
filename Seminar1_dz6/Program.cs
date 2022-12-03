6b7770e3009425dd32ad15aa659005e6aa02c592
Yana <petersyana@rambler.ru>
2022-12-03 19:07:33 +0300

    решение проблемы в сист упр версиями

---
+++ Seminar1_dz6/Program.cs
@@ -0,0 +1,16 @@
+﻿// программа определяет четное число или нечетное
+// 4 -3 7
+
+Console.WriteLine("Введите число - ");
+string enterNum = Console.ReadLine();
+int num = Convert.ToInt32(enterNum);
+
+if (num%2 == 0)
+    {
+        Console.WriteLine($"{num} - четное число");
+    }
+else
+    {
+        Console.WriteLine($"{num} - нечетное число");
+    }
+
