e93c32663973b25a0fdc0386b07127f6bf00d162
Yana <petersyana@rambler.ru>
2022-12-05 21:56:55 +0300

    task2

---
+++ Seminar2_task2/Program.cs
@@ -0,0 +1,18 @@
+﻿// программа, которая считает расстояние между двумя точками по их координатам
+
+Console.Write("Для точки А введите значение координаты Х=  ");
+int Xa = int.Parse(Console.ReadLine());
+
+Console.Write("Для точки А введите значение координаты Y=  ");
+int Ya = int.Parse(Console.ReadLine());
+  
+Console.Write("Для точки B введите значение координаты Х=  ");
+int Xb = int.Parse(Console.ReadLine());
+
+Console.Write("Для точки B введите значение координаты Y=  ");
+int Yb = int.Parse(Console.ReadLine());
+
+double AB = Math.Sqrt(Math.Pow((Xb - Xa),2) + Math.Pow((Yb - Ya),2));
+
+Console.Write($"Расстояние между точками АВ = {AB}");
+
