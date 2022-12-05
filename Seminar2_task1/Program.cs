9ea6b8f1296e4261b937fc42ba2327657f9526a6
Yana <petersyana@rambler.ru>
2022-12-05 21:56:45 +0300

    task1

---
+++ Seminar2_task1/Program.cs
@@ -0,0 +1,28 @@
+﻿// программа, которая по номеру четверти (плоскость координат) выдаст 
+// Х и У
+
+Console.WriteLine("введите номер четверти в плоскости координат (от 1 до 4) - ");
+int num = int.Parse(Console.ReadLine());
+
+switch(num)
+{
+    case 1:
+        Console.WriteLine("значения координат будут в диапазоне: Х>0, Y>0");
+    break;
+
+    case 2:
+        Console.WriteLine("значения координат будут в диапазоне: Х<0, Y>0");
+    break;
+
+    case 3:
+        Console.WriteLine("значения координат будут в диапазоне: Х<0, Y<0");
+    break;
+
+    case 4:
+        Console.WriteLine("значения координат будут в диапазоне: Х>0, Y<0");
+    break;
+
+    default:
+        Console.WriteLine("В координатной плоскости 4 четверти. Введите число от 1 до 4 ");
+    break;
+}
