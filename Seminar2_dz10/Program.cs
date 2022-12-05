bb6f1594d75c19ab72def91995143ffdd8520666
Yana <petersyana@rambler.ru>
2022-12-05 17:03:34 +0300

    dz10

---
+++ Seminar2_dz10/Program.cs
@@ -0,0 +1,7 @@
+﻿// программа принимает на входе трехзначное число и на выходе показывает 2 цифру этого числа
+
+Console.WriteLine("Введите трехзначное число");
+int num = int.Parse(Console.ReadLine());
+int num1 = num % 100;
+int num2 = (num1 - num % 10)/10;
+Console.WriteLine(num2);
