dab882e42b5322da267e23bef30bfcce2bc27a42
Yana <petersyana@rambler.ru>
2022-12-05 17:06:26 +0300

    add text dz10

--- Seminar2_dz10/Program.cs
+++ Seminar2_dz10/Program.cs
@@ -4,4 +4,4 @@ Console.WriteLine("Введите трехзначное число");
 int num = int.Parse(Console.ReadLine());
 int num1 = num % 100;
 int num2 = (num1 - num % 10)/10;
-Console.WriteLine(num2);
+Console.WriteLine($"в числе {num} вторая цифра {num2}");
