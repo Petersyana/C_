db349419c7d06c11148b3868fb2d4053a63a9a7b
Yana <petersyana@rambler.ru>
2022-12-05 18:41:01 +0300

    dz13

---
+++ Seminar2_dz13/Program.cs
@@ -0,0 +1,15 @@
+﻿// программа, которая выводит третью цифру заданного числа или пишет, что такой цифры нет
+
+Console.WriteLine("Введите любое число - ");
+string enterNum = Console.ReadLine();
+
+int amount = enterNum.Length; // кол-во символов(цифр) в числе
+if (amount < 3)  
+    {
+    Console.WriteLine($"В данном числе {enterNum} кол-во цифр меньше трех");
+    }
+else
+    {
+    Console.Write($"В данном числе {enterNum} третья по счету цифра - ");
+    Console.WriteLine(enterNum.Substring(2, 1));
+    }
