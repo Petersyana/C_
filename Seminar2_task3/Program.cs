3fdaccd56d75abefd443eef0567f501babd45e9a
Yana <petersyana@rambler.ru>
2022-12-05 21:57:04 +0300

    task3

---
+++ Seminar2_task3/Program.cs
@@ -0,0 +1,15 @@
+﻿// программа получает на вводе число N и выдает квадраты всех чисел от 1 до этого N
+
+Console.Clear();
+Console.WriteLine("Введите число");
+
+int num = int.Parse(Console.ReadLine());
+int count = 1;
+double result = 0;
+
+while (count < num) 
+    {
+       result = Math.Pow(count,2); 
+        Console.WriteLine(result); 
+        count++;       
+    }
