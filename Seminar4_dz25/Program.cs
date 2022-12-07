08036b5839accd8f2282bc5204fc1538aae5c064
Yana <petersyana@rambler.ru>
2022-12-07 21:51:52 +0300

    add dz25

---
+++ Seminar4_dz25/Program.cs
@@ -0,0 +1,15 @@
+﻿// программа принимает на вход числа А и В и выдает число А в степени В
+
+Console.WriteLine("Введите число А - ");
+int numA = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите число B - ");
+int numB = int.Parse(Console.ReadLine());
+
+int result = numA;
+
+for (int i = 1; i < numB; i++)
+    {
+        result = result*numA;
+    }
+Console.WriteLine($"число A={numA} в степени B={numB} = {result}");
