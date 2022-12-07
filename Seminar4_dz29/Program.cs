d7cb8a139ffccad33435d6907d74defba2ee861e
Yana <petersyana@rambler.ru>
2022-12-07 21:52:19 +0300

    add dz29

---
+++ Seminar4_dz29/Program.cs
@@ -0,0 +1,19 @@
+﻿// программа задает массив из 8 элементов и выдает их на экран
+
+Console.WriteLine("Введите 8 чисел");
+int[] arr = new int[8]; // задаем массив
+
+for (int i=0; i<8; i++)
+    {
+        arr[i] = int.Parse(Console.ReadLine());
+    }
+Console.WriteLine(string.Join(",", arr));
+
+// генерация массива
+// int[] arr = new int[8];
+// var rnd = new Random();
+// for (int i=0; i<8; i++)
+//     {
+//         arr[i] = rnd.Next(0,100);
+//     }
+// Console.WriteLine(string.Join(",", arr));
