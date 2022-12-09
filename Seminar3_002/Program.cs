e2b21da7017b4fd6ef7a89d661b61ae59bcc76e1
Yana <petersyana@rambler.ru>
2022-12-09 21:55:50 +0300

    Seminar3_002

---
+++ Seminar3_002/Program.cs
@@ -0,0 +1,11 @@
+﻿// программа считает произведение всех чисел от 1 до N
+
+System.Console.WriteLine("Введите число N  ");
+int num = int.Parse(Console.ReadLine());
+int work = 1;
+
+for (int i = 1; i <= num; i++)
+{
+    work = work*i;
+}
+Console.WriteLine(work);
