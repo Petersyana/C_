6200199cb914de93eeba200a7f7fdde7388d4f8d
Yana <petersyana@rambler.ru>
2022-12-09 21:55:36 +0300

    Seminar3_003

---
+++ Seminar3_003/Program.cs
@@ -0,0 +1,24 @@
+﻿// программа, кото выводит массив из 8 эелементов,
+// заполненный 0 и 1 в произвольном порядке
+
+// Console.WriteLine();
+// int[] arr = new int[8];
+// int i = 0;
+
+// for (; i<8; i++)
+// {
+//     var rnd = new Random();
+//     arr[i] = rnd.Next(0,2);
+// }
+// //Console.Write(arr[i]+"_");
+
+// Console.WriteLine(stringJoin(",",arr));
+
+int[] array = new int[8];
+var random = new Random();
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = random.Next(0, 2);
+}
+Console.WriteLine(string.Join(", ", array));
