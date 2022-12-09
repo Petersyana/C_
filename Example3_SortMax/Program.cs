f8cb178ed4c198133cc70a35a877ac05c2078fc8
Yana <petersyana@rambler.ru>
2022-12-09 21:57:29 +0300

    Example3_SortMax

---
+++ Example3_SortMax/Program.cs
@@ -0,0 +1,31 @@
+﻿// программа упорядочивает массив от мах к мин
+
+int[] arr = {3,8,7,6,14,9,5};
+
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length-1; i++)
+    {
+        int maxPosition = 0;
+        for (int j = i; j < array.Length; j++)
+        {
+            if (array[j] > array[maxPosition]) maxPosition = j;            
+        }
+    int temp = array[i];
+    array[i] = array[maxPosition];
+    array[maxPosition] = temp;
+    }
+}
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+        for (int i = 0; i < count; i++)
+        {
+            Console.Write($"{array[i]} ");
+        }
+Console.WriteLine();
+}
+
+SelectionSort(arr);
+PrintArray(arr);
