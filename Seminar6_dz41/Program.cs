957d3faa78967e5dd96c7344d163449785d6d71c
Yana <petersyana@rambler.ru>
2022-12-10 22:21:58 +0300

    add dz41

---
+++ Seminar6_dz41/Program.cs
@@ -0,0 +1,15 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Сколько введете чисел? - ");
+int n = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Введите {n} чисел через Enter" );
+
+int count = 0;
+for (int i=0; i<n; i++)
+{
+if (int.Parse(Console.ReadLine()) > 0) count=count+1;
+}
+Console.WriteLine($" Кол-во чисел > 0 = {count}");
