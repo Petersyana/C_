d0b1c0805bbaccce5048154ed427efed746b1758
Yana <petersyana@rambler.ru>
2022-12-10 22:21:11 +0300

    task41

---
+++ Seminar6_task41/Program.cs
@@ -0,0 +1,21 @@
+﻿// Программа, которая переводит десятичное число в двоичное
+
+Console.WriteLine("Введите число - ");
+int num = int.Parse(Console.ReadLine());
+int i=0;
+int res = 0;
+int ostatok = 0;
+
+
+while (num > 0)
+{
+    ostatok = num%2;
+    num = num/2;
+    
+    Console.Write(ostatok);
+    
+}
+
+//Console.WriteLine(res);
+    
+
