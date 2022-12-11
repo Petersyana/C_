e99c41fe40ec01e3ec488f436e93bc74e593e687
Yana <petersyana@rambler.ru>
2022-12-11 20:52:24 +0300

    add task41

--- Seminar6_task41/Program.cs
+++ Seminar6_task41/Program.cs
@@ -2,20 +2,54 @@
 
 Console.WriteLine("Введите число - ");
 int num = int.Parse(Console.ReadLine());
-int i=0;
-int res = 0;
+
+int print_num = num; // запоминаем число для вывода на печать в результате вычислений
+int count = 0; // считаем длину массива из 0 и 1
+int temp_num = num; // доп переменная для подсчета длины массива
+
+
+// доп цикл для подсчета длины массива с 0 и 1
+while (temp_num > 0) 
+{
+    temp_num = temp_num/2;
+    count ++;
+}
+
+
+int i = 0;
+int [] res = new int[count];
 int ostatok = 0;
 
+// цикл переводит десятичное число в двоичное
 
-while (num > 0)
+while (num > 0) 
 {
     ostatok = num%2;
     num = num/2;
-    
-    Console.Write(ostatok);
-    
+    res[i] = ostatok; // массив из 0 и 1
+    i ++;
 }
 
-//Console.WriteLine(res);
+// метод переворачивает массив
+
+ int[] PerevorotArray(int[] array)
+ {
+    int temp = array[0];
+
+    for (int j = 0; j < array.Length/2; j++)
+       {
+        temp = array[j];
+        array[j] = array[array.Length-1-j];
+        array[array.Length-1-j] = temp;
+        }
+    return array;
+    Console.WriteLine();
+}
+
+  PerevorotArray(res); // вызов метода переворота
+ 
+Console.Write($"Число {print_num} в десятичном формате - "); 
+Console.Write(String.Join(" ",res));
+
     
 
