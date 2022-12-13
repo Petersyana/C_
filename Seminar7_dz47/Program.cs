3e5d6ef2de89bb1328ce4e969556d27ef0901308
Yana <petersyana@rambler.ru>
2022-12-13 19:58:34 +0300

    add dz47

---
+++ Seminar7_dz47/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+Console.Write("Введите количество строк массива m= ");
+int m = int.Parse(Console.ReadLine());
+
+Console.Write("Введите количество столбцов массива n= ");
+int n = int.Parse(Console.ReadLine());
+
+int[,] array = new int[m,n];
+
+for (int i =0; i<m; i++)
+{
+    for (int j=0; j<n; j++)
+   {
+    array[i,j] = new Random().Next(10,99);
+    }
+}
+Console.WriteLine();
+
+void PrintArray(int[,] inArray) // метод печати массива
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+PrintArray(array);
