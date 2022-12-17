c6a9d78e4d28c349d0085d363fd5b23e8ff52200
Yana <petersyana@rambler.ru>
2022-12-17 22:33:08 +0300

    add oprosnik

--- Seminar8_oprosnik/Program.cs
+++ Seminar8_oprosnik/Program.cs
@@ -6,9 +6,15 @@ int count = 0; // счетчик вопросов
 int num = 0; // номер ответа
 
 string[] Question = new string[]{"Содержание вопроса1", "Содержание вопроса2", "Содержание вопроса3"};
-string[,] variantAnswer = new string[3,4]{{"Ответ1", "Ответ2", "Ответ3", "Ответ4"}, {"Ответ1", "Ответ2", "Ответ3", "Ответ4"}, {"Ответ1", "Ответ2", "Ответ3", "Ответ4"}};
-bool [,] etalonAnswer = new bool[3,4]{{false, true, false, false}, {false, false, true, false}, {true, false, false, false}};
-bool [,] Answer = new bool[3,4]{{false, false, false, false}, {false, false, false, false}, {false, false, false, false}};
+
+string[,] variantAnswer = new string[3,4]{{"Ответ1", "Ответ2", "Ответ3", "Ответ4"}, 
+{"Ответ1", "Ответ2", "Ответ3", "Ответ4"}, {"Ответ1", "Ответ2", "Ответ3", "Ответ4"}};
+
+bool [,] etalonAnswer = new bool[3,4]{{false, true, false, false}, 
+{false, false, true, false}, {true, false, false, false}};
+
+bool [,] Answer = new bool[3,4]{{false, false, false, false}, 
+{false, false, false, false}, {false, false, false, false}};
 
 
 int i = 0;
