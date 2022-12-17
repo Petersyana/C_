ab6183050cbad3a9ae483aba8f80b13e1f11ac91
Yana <petersyana@rambler.ru>
2022-12-17 22:33:39 +0300

    add 8_calcul

---
+++ Seminar8_calcul/Program.cs
@@ -0,0 +1,12 @@
+﻿// простой калькулятор
+
+Console.WriteLine("Введите математическое выражение - ");
+string str = (Console.ReadLine());
+
+string[] separators = {"+","-","*","/","="};
+
+string[] subs = str.Split(separators, StringSplitOptions.RemoveEmptyEntries);
+Console.WriteLine(subs[0]);
+
+
+
