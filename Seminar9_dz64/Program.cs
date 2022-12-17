5e9f34896b998cf6d5e171900130fa096c3b3ed7
Yana <petersyana@rambler.ru>
2022-12-17 22:30:30 +0300

    add dz64

---
+++ Seminar9_dz64/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа 
+// в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+Console.WriteLine("Введите число ");
+int num = int.Parse(Console.ReadLine());
+
+/* for(int i=num; i>=1; i--)
+Console.Write($" {i}"); */
+
+
+int Vivod(int num)
+{
+    Console.Write($" {num}");
+    if(num<1) return 0;
+    else return Vivod(num-1);
+}
+
+Vivod(num);
