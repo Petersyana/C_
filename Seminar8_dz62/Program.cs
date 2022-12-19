7983ffb2f94c68a68fb2643aaff83f1079faa392
Yana <petersyana@rambler.ru>
2022-12-19 18:41:53 +0300

    все сначала буду делать

--- Seminar8_dz62/Program.cs
+++ Seminar8_dz62/Program.cs
@@ -5,80 +5,13 @@ int x = 0;
 int y = 0;
 
 int[,] array = new int[n,n]; //создаем массив из 0
-for (x = 0; x < n; x++)
-{
-    for (y = 0; y < n; y++)
-    {
-        array[x,y] = 0;
-    }
-}
-
-int num = 1;
-x = 0; 
-y = -1;
-int d_column = 1; //-1 0 1
-int d_row = 0; // -1 0 1
-
-/* int spiral(int x, int y, int d_row, int d_column)
-{
-    if ((0 <= x+d_row & x+d_row < n) & (0 <= y+d_column & y+d_column < n) & (array[x+d_row, y+d_column] == 0))
-    {     
-    x += d_row;
-    y += d_column;
-    Console.WriteLine($"{x},{y}");
-    array[x,y] = num;
-    num +=1;
-    }
-   return (Convert.ToInt32(x), Convert.ToInt32(y), Convert.ToInt32(d_row), Convert.ToInt32(d_column));
-) */
-
-while(num < Math.Pow(n,2))
-{
-    //spiral(x, y, d_row, d_column);
-    if ((0 <= x+d_row && x+d_row < n) && 
-    (0 <= y+d_column && y+d_column < n) && 
-    (array[x+d_row, y+d_column] == 0))
-    {     
-    x += d_row;
-    y += d_column;
-    Console.WriteLine($"{x},{y}");
-    array[x,y] = num;
-    num +=1; 
-    }
-    else
-    {
-    if (d_column == 1)
-        {
-            d_column = 0;
-            d_row = 1;
-            Console.WriteLine("first");
-        //}
-else
-        if(d_row == 1)
-           {
-           d_column = -1;
-            d_row = 0; 
-           //}
-    else
-            if(d_column == -1)
-                {
-                    d_column = 0;
-                    d_row = -1;
-              //  }
-         else
-                if(d_row == -1)
-                {
-                d_column = 1;
-                    d_row = 0; 
-                } 
-                }
-           }
-        }
-     }
-}
-            
-            
-          
+// for (x = 0; x < n; x++)
+// {
+//     for (y = 0; y < n; y++)
+//     {
+//         array[x,y] = 0;
+//     }
+// }
 
 void PrintArray(int[,] inArray) // вывод на печать
 {
@@ -91,5 +24,83 @@ void PrintArray(int[,] inArray) // вывод на печать
         Console.WriteLine();
     }
 }
+
+//PrintArray(array);
+
+int num = 1;
+int n1 = 1;
+x = 0; 
+y = 1;
+int d_column = 1; //-1 0 1
+int d_row = 0; // -1 0 1
+
+
+// while(n1 < n*2)
+// {
+//     if ((x <= n) && (y <= n))
+//     {
+//     array[x,y] = num;
+//     Console.Write($" {num}");
+//     x = x + d_row;
+//     y = y + d_column;
+//     num = num+1;
+    
+//     }
+// //}
+
+
+
+
+
+
+
+
+
+
+
+    if ((0 <= x+d_row && x+d_row < n) && 
+    (0 <= y+d_column && y+d_column < n) && 
+    (array[x+d_row, y+d_column] == 0))
+    {     
+        x += d_row;
+        y += d_column;
+        Console.Write($" {num}");
+        array[x,y] = num;
+        num +=1; 
+    }
+
+    //  if (d_column == 1)
+    //     {
+    //         d_column = 0;
+    //         d_row = 1;
+    //         Console.Write($" {num}");
+    //      }
+// }
+// else
+//         if(d_row == 1)
+//            {
+//            d_column = -1;
+//             d_row = 0; 
+//            //}
+//     else
+//             if(d_column == -1)
+//                 {
+//                     d_column = 0;
+//                     d_row = -1;
+//               //  }
+//          else
+//                 if(d_row == -1)
+//                 {
+//                 d_column = 1;
+//                     d_row = 0; 
+//                 } 
+//                 }
+//            }
+//         }
+//      }
+// }
+            
+            
+          
 
 PrintArray(array);
